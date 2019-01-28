47719d0ae3c0f45d3bbac830cdc409b14b3f10bf
chenguilong <chenguilong@julong.cc>
2019-01-28 15:04:02 +0800

    first commit

---
+++ src/main/java/com/muzhu/muzhu/MuzhuApplication.java
@@ -0,0 +1,14 @@
+package com.muzhu.muzhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzhuApplication.class, args);
+	}
+
+}
+
